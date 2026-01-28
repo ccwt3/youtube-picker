f73440c07a97e4dfd9c0306139d989a7ae09d8da
MoyyyL <moyyyl0526@gmail.com>
2026-01-28 17:10:33 -0600

    now i added the youtube api

--- app/api/getNovia/route.ts
+++ app/api/getNovia/route.ts
@@ -1,10 +1,15 @@
 import { NextResponse } from "next/server";
+import "dotenv/config";
 
 export async function GET(request: Request) {
-  const data = {
-    nombre: "omar",
-    admin: false,
-  };
+  const query = "philosophy";
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&relevanceLanguage=en&type=video&videoDuration=medium&maxResults=5&key=${process.env.API_KEY}`;
 
-  return NextResponse.json(data, { status: 200 });
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    console.error("error al consultar la API: ", err);
+  }
 }
