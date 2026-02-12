a97dbf565d24606d93fe45f428609f65c213a1a4
MoyyyL <moyyyl0526@gmail.com>
2026-02-12 09:32:26 -0600

    we can get the videos from the database woohoo

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,6 +1,33 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
-export async function GET(request: Request) {
-  return NextResponse.json("hola");
+async function randomVideo() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("video").select(`
+    id,
+    videoUrl,
+    video_tags (
+      tags (
+        id,
+        tag_name
+      )
+    )
+  `);
+
+  if (error) console.error(error);
+  return data;
+}
+
+export async function GET(request: NextRequest) {
+  const parameters = request.nextUrl.searchParams;
+
+  const filters = parameters.getAll("topic");
+  const timeSnaps = parameters.getAll("timesnap");
+
+  if (filters.length === 0 && timeSnaps.length === 0) {
+    const video = await randomVideo();
+    return NextResponse.json(video);
+  }
+
+  return NextResponse.json("osaka");
 }
--- lib/handleGetVideo.ts
+++ lib/handleGetVideo.ts
@@ -1,13 +1,32 @@
 "use client";
 
+interface timeTopic {
+  time: string[];
+  filter: string[];
+}
+
 export async function getVideo() {
   const rawData = localStorage.getItem("filters");
-  let filterData;
+  let filterData: timeTopic;
   if (rawData) {
     filterData = JSON.parse(rawData);
+  } else {
+    filterData = { time: [], filter: [] };
   }
 
-  const res = await fetch("/api/video");
+  let query = "";
+
+  filterData.time.forEach((t) => {
+    query += `&timesnap=${t}`;
+  });
+
+  filterData.filter.forEach((f) => {
+    query += `&topic=${f}`;
+  });
+
+  console.log(query);
+
+  const res = await fetch(`/api/video?${query}`);
   const data = await res.json();
 
   return console.log(data);
